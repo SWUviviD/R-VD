931da30354361f42fa7879e33d25f1e4ed3be83f
ANT <bit0316@naver.com>
2024-10-07 21:57:48 +0900

    [Chore] 맵툴 제작에 사용할 샘플 프리팹 추가

---
+++ Assets/Scripts/LevelEditor/LevelEditorCamera.cs
@@ -0,0 +1,25 @@
+#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditor
+{
+    public class LevelEditorCamera : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+#endif

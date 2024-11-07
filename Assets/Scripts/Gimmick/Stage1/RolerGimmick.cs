9733e951d4a52ed10ee51a900b8a35153bf5e83b
ioctnt18 <ioctnt18@gmail.com>
2024-11-08 08:10:50 +0900

    [Feat] 일반기믹 빙글빙글 rg 오류 해결, 낙리스폰 논의 요망

--- Assets/Scripts/Gimmick/Stage1/RolerGimmick.cs
+++ Assets/Scripts/Gimmick/Stage1/RolerGimmick.cs
@@ -4,36 +4,48 @@ using UnityEngine;
 public class RolerGimmick : GimmickBase<RolerGimmickData>
 {
     private float rotationSpeed;
+    private Vector3 initialPosition;
     private Rigidbody rb;
-
 
     protected override void Init()
     {
         // 추가 작업 없음
     }
 
-
     public override void SetGimmick()
     {
+        LogManager.Log("rotationDuration before: " + gimmickData.rotationDuration);
         rotationSpeed = 360f / gimmickData.rotationDuration;
+        LogManager.Log("rotationSpeed after: " + rotationSpeed);
     }
 
     private void Start()
     {
+        SetGimmick();
+
+        // 초기 위치를 오브젝트의 현재 위치로 설정
+        initialPosition = transform.position;
+
+        // Rigidbody 컴포넌트 가져오기
         rb = GetComponent<Rigidbody>();
-        StartCoroutine(RotateObject());
+
+        // 로그로 회전 속도 및 초기 위치 확인
+        LogManager.Log("rotationSpeed: " + rotationSpeed);
+        LogManager.Log("initialPosition: " + initialPosition);
     }
 
-
-    private IEnumerator RotateObject()
+    private void FixedUpdate()
     {
+        // 시계방향/반시계방향 설정
         float direction = gimmickData.rotateClockwise ? 1f : -1f;
 
-        while (true)
-        {
-            rb.angularVelocity = new Vector3(0, 0, direction * rotationSpeed * Mathf.Deg2Rad);
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, direction * rotationSpeed * Time.fixedDeltaTime, 0));
 
-            yield return null;
-        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        // 충돌 중일 때, 오브젝트를 원래 위치로 고정
+        transform.position = initialPosition;
     }
 }

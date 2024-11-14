91ea93560ecb3393a583a1842d2cb2f69764097b
ioctnt18 <ioctnt18@gmail.com>
2024-11-14 16:58:23 +0900

    [Fix] 별똥별 추적 1블럭 완성 (넉백 x)

--- Assets/Scripts/Gimmick/Stage1/ChasingGimmickData.cs
+++ Assets/Scripts/Gimmick/Stage1/ChasingGimmickData.cs
@@ -6,11 +6,11 @@ public class ChasingGimmickData : GimmickDataBase
 {
     [GimmickData("별똥별이 떨어지는 시간 (개당)")]
     [field: SerializeField]
-    public float StarFallSpeed { get; set; } = 2f;
+    public float StarFallSpeed { get; set; } = 4f;
 
     [GimmickData("별똥별의 리스폰 간격")]
     [field: SerializeField]
-    public float StarShowInterval { get; set; } = 2f;
+    public float ResponeTime { get; set; } = 2f;
 
     [GimmickData("충돌 시 플레이어의 데미지량")]
     [field: SerializeField]

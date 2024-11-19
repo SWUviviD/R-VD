cb0d07abcec9abebdcec3f634c1f8451adb9d48d
YB970902 <78996787+YB970902@users.noreply.github.com>
2024-11-20 00:35:14 +0900

    [Feat] 카메라가 경로를 따라가는 기능 분리

--- Assets/Scripts/LevelEditor/CameraPath/CameraPathInsertSystem.cs
+++ Assets/Scripts/LevelEditor/CameraPath/CameraPathInsertSystem.cs
@@ -9,13 +9,15 @@ using UnityEngine.UI;
 
 /// <summary>
 /// 카메라 경로를 의미하는 포인트의 정보
+/// 하나의 경로는 베지어 곡선을 위한 포인트 1~4를 갖는다.
+/// 이전 경로의 4번 포인트는 다음 경로의 1번 포인트와 이어진다. 
 /// </summary>
 public class CameraPathPoint
 {
     /// <summary> 포인트의 위치 </summary>
     public Vector3 Position { get; set; }
     
-    // 베지어 곡선에 필요한 값들
+    // 베지어 곡선에 필요한 값. 각 순서별로 경로의 시작위치에 가깝다.
     public Vector3 PointV1 { get; set; }
     public Vector3 PointV2 { get; set; }
     public Vector3 PointV3 { get; set; }
@@ -41,7 +43,7 @@ public class CameraPathPoint
     }
 }
 
-public class CameraPathInsertSystem : MonoBehaviour
+public class CameraPathInsertSystem : MonoSingleton<CameraPathInsertSystem>
 {
     [SerializeField] private CameraPathInputSystem inputSystem;
     [SerializeField] private CameraPointCollider prefabCameraPoint;
@@ -50,9 +52,6 @@ public class CameraPathInsertSystem : MonoBehaviour
     [SerializeField] private LineRenderer pathRenderer;
     
     [SerializeField] private Text txtState;
-
-    [SerializeField] private Transform trPlayerPoint;
-    [SerializeField] private float cameraDist;
     
     public GimmickDefines.CameraPathInsertMode InsertMode { get; private set; }
 
@@ -80,10 +79,11 @@ public class CameraPathInsertSystem : MonoBehaviour
             // ESC키를 누르면 현재 핸들의 위치 수정하는 오브젝트를 끈다.
             OnClickPathHandle(null);
         }
+    }
 
-        if (trPlayerPoint.gameObject.activeSelf == false) return;
-
-        Camera.main.transform.position = GetCameraPosition(trPlayerPoint.position, cameraDist);
+    public List<CameraPathPoint> GetCameraPath()
+    {
+        return CameraPointList.ConvertAll(_ => _.CameraPathPoint);
     }
 
     public void SetInsertMode(GimmickDefines.CameraPathInsertMode _insertMode)
@@ -160,124 +160,5 @@ public class CameraPathInsertSystem : MonoBehaviour
                 ratio += incrementRatio;
             }
         }
-    }
-
-    /// <summary>
-    /// 플레이어의 위치와 거리에 따른 카메라의 위치를 계산하는 함수. 
-    /// </summary>
-    public Vector3 GetCameraPosition(Vector3 _playerPosition, float _dist)
-    {
-        int prevIndex = -1;
-        int currIndex = -1;
-
-        Vector2 playerPoint = GetCameraPoint(_playerPosition);
-        
-        for (int i = 0; i < CameraPointList.Count; ++i)
-        {
-            var point = CameraPointList[i];
-            currIndex = i;
-
-            if (IsInCircle(GetCameraStartPoint(point.CameraPathPoint), playerPoint, _dist) == false &&
-                IsInCircle(GetCameraEndPoint(point.CameraPathPoint), playerPoint, _dist))
-            {
-                // 카메라의 위치가 현재 카메라 포인트의 시작점과 끝점 사이에 있는 경우
-                if (IsInCircle(GetCameraPoint(point.CameraPathPoint.PointV1), playerPoint, _dist) == false &&
-                    IsInCircle(GetCameraPoint(point.CameraPathPoint.PointV2), playerPoint, _dist))
-                {
-                    return GetCameraPath(point.CameraPathPoint.PointV1, point.CameraPathPoint.PointV2, _playerPosition, _dist);
-                }
-                if (IsInCircle(GetCameraPoint(point.CameraPathPoint.PointV2), playerPoint, _dist) == false &&
-                    IsInCircle(GetCameraPoint(point.CameraPathPoint.PointV3), playerPoint, _dist))
-                {
-                    return GetCameraPath(point.CameraPathPoint.PointV2, point.CameraPathPoint.PointV3, _playerPosition, _dist);
-                }
-                if (IsInCircle(GetCameraPoint(point.CameraPathPoint.PointV3), playerPoint, _dist) == false &&
-                    IsInCircle(GetCameraPoint(point.CameraPathPoint.PointV4), playerPoint, _dist))
-                {
-                    return GetCameraPath(point.CameraPathPoint.PointV3, point.CameraPathPoint.PointV4, _playerPosition, _dist);
-                }
-            }
-
-            if (prevIndex >= 0 && IsInCircle(GetCameraStartPoint(point.CameraPathPoint), playerPoint, _dist))
-            {
-                // 카메라가 이전 마지막 지점과 현재 첫 번째 지점 사이에 있는경우
-                var prevPoint = CameraPointList[prevIndex];
-                return GetCameraPath(prevPoint.CameraPathPoint.PointV4, point.CameraPathPoint.PointV1, _playerPosition, _dist);
-            }
-
-            prevIndex = currIndex;
-        }
-
-        return CameraPointList[^1].CameraPathPoint.PointV4;
-    }
-
-    private Vector3 GetCameraPath(Vector3 _start, Vector3 _end, Vector3 _circle, float _radius)
-    {
-        // 카메라가 이전 마지막 지점과 현재 첫 번째 지점 사이에 있는경우
-        var point1 = GetCameraPoint(_start);
-        var point2 = GetCameraPoint(_end);
-        var circle = GetCameraPoint(_circle);
-        float ratio = GetLineCircleIntersection(point1, point2, circle, _radius);
-        return _start + (_end - _start) * ratio;
-    }
-
-    /// <summary>
-    /// 카메라 포인트의 위치를 Vector2로 구한다. Y축을 제외한 X, Z축이다.
-    /// </summary>
-    private Vector2 GetCameraPoint(Vector3 _point)
-    {
-        return new Vector2(_point.x, _point.z);
-    }
-    
-    private Vector2 GetCameraStartPoint(CameraPathPoint _pathPoint)
-    {
-        return GetCameraPoint(_pathPoint.PointV1);
-    }
-
-    private Vector2 GetCameraEndPoint(CameraPathPoint _pathPoint)
-    {
-        return GetCameraPoint(_pathPoint.PointV4);
-    }
-
-    private bool IsInCircle(Vector2 _position, Vector2 _circleCenter, float _circleRadius)
-    {
-        return (_position - _circleCenter).magnitude < _circleRadius;
-    }
-
-    private float GetLineCircleIntersection(Vector2 _point1, Vector2 _point2, Vector2 _circleCenter, float _circleRadius)
-    {
-        List<Vector2> intersections = new List<Vector2>();
-
-        // 직선의 방향 벡터
-        Vector2 d = _point2 - _point1;
-
-        // 이차방정식의 계수 계산
-        float dx = d.x;
-        float dy = d.y;
-
-        float A = dx * dx + dy * dy;
-        float B = 2 * (dx * (_point1.x - _circleCenter.x) + dy * (_point1.y - _circleCenter.y));
-        float C = (_point1.x - _circleCenter.x) * (_point1.x - _circleCenter.x) + (_point1.y - _circleCenter.y) * (_point1.y - _circleCenter.y) - _circleRadius * _circleRadius;
-
-        float discriminant = B * B - 4 * A * C;
-
-        if (discriminant == 0)
-        {
-            // 접점 하나
-            return -B / (2 * A);
-        }
-        
-        if(discriminant > 0)
-        {
-            // 교점 두 개
-            float sqrtDiscriminant = Mathf.Sqrt(discriminant);
-
-            float t1 = (-B + sqrtDiscriminant) / (2 * A);
-            float t2 = (-B - sqrtDiscriminant) / (2 * A);
-
-            return Mathf.Min(t1, t2);
-        }
-
-        return 1f;
     }
 }

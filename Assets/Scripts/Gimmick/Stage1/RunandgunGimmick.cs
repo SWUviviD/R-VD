13c45b10927157c124ee3cd302934f6da5e420f7
ioctnt18 <ioctnt18@gmail.com>
2024-11-21 17:03:51 +0900

    [Del] PlayerStatus 수정 중 런앤건 코드 오류로 재작성을 위한 스크립트 삭제

--- Assets/Scripts/Gimmick/Stage1/RunandgunGimmick.cs
+++ Assets/Scripts/Gimmick/Stage1/RunandgunGimmick.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 
 public class RunandgunGimmick : GimmickBase<RunandgunGimmickData>
 {
-    [SerializeField] private LayerMask damageLayerMask; // 데미지 존 레이어 마스크
-    [SerializeField] private LayerMask healLayerMask;   // 힐 존 레이어 마스크
     [SerializeField] private PlayerStatus playerStatus; // 플레이어 상태를 관리하는 스크립트 참조
 
 
     /// <summary>
     /// 변수 선언
     /// </summary>
-    private float elapsedTime;  // 경과 시간 변수
 
     /// <summary>
     /// 상속
@@ -25,38 +22,6 @@ public class RunandgunGimmick : GimmickBase<RunandgunGimmickData>
 
     public override void SetGimmick()
     {
-        elapsedTime = 0f;
-    }
-
-
-    /// <summary>
-    /// 기믹 작동
-    /// </summary>
-        private void Update()
-    {
-        // 플레이어 아래에 Ray를 쏘아 현재 레이어 확인
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, Mathf.Infinity))
-        {
-            // DamageZone일 경우 체력 감소
-            if ((damageLayerMask.value & (1 << hit.collider.gameObject.layer)) > 0)
-            {
-                elapsedTime += Time.deltaTime;
-
-                if (elapsedTime >= gimmickData.DamageTickInterval)
-                {
-                    playerStatus.Damage(gimmickData.DamageAmount); // 체력 감소 함수 호출
-                    elapsedTime = 0f;                     // 경과 시간 초기화
-                }
-            }
-            // HealZone일 경우 체력 회복
-            else if ((healLayerMask.value & (1 << hit.collider.gameObject.layer)) > 0)
-            {
-                playerStatus.FullHeal(); // 플레이어 체력 회복
-            }
-            else
-            {
-                elapsedTime = 0f; // 어떤 존에도 해당하지 않으면 경과 시간 초기화
-            }
-        }
+        
     }
 }

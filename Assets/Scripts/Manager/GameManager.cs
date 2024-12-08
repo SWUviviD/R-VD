f5d66f603fb75204477069da90d2988375185396
YB970902 <78996787+YB970902@users.noreply.github.com>
2024-12-08 14:05:16 +0900

    [Feat] 에디터 시작 시 널에러 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -19,24 +19,32 @@ public class GameManager : MonoSingleton<GameManager>
 
     private void Awake()
     {
-        // 맵을 로드한다.
-        Player = MapLoadManager.Instance.LoadMap("map");
-        LDMapData mapData = MapLoadManager.Instance.MapData;
-
-        if (Player != null)
+#if UNITY_EDITOR
+        if (SceneManager.GetActiveScene().name != "LevelEditor")
         {
-            var cameraController = CameraController.Instance;
-            // 카메라가 플레이어를 따라가도록 한다.
-            cameraController.SetPlayer(Player.transform);
-            // 카메라 경로를 넣고 동작시킨다.
-            cameraController.Set(mapData.CameraPathList.ConvertAll(_ => _.ToCameraPathPoint()));
-            cameraController.Play();
+#endif
+            // 맵을 로드한다.
+            Player = MapLoadManager.Instance.LoadMap("map");
+            LDMapData mapData = MapLoadManager.Instance.MapData;
 
-            Player.GetComponent<PlayerHp>().OnDeath.RemoveListener(OnGameOver);
-            Player.GetComponent<PlayerHp>().OnDeath.AddListener(OnGameOver);
+            if (Player != null)
+            {
+                var cameraController = CameraController.Instance;
+                // 카메라가 플레이어를 따라가도록 한다.
+                cameraController.SetPlayer(Player.transform);
+                // 카메라 경로를 넣고 동작시킨다.
+                cameraController.Set(mapData.CameraPathList.ConvertAll(_ => _.ToCameraPathPoint()));
+                cameraController.Play();
+
+                Player.GetComponent<PlayerHp>().OnDeath.RemoveListener(OnGameOver);
+                Player.GetComponent<PlayerHp>().OnDeath.AddListener(OnGameOver);
+            }
+
+            OnGameStart();
+#if UNITY_EDITOR
         }
+#endif
 
-        OnGameStart();
         ResumeGame();
     }
 

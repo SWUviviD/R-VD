c36feca5f9cd793ecd17a88af8b9d0b0b4aafaf3
csy-59 <csy010509@naver.com>
2025-08-12 19:02:42 +0900

    [Feat]시로 날개 회전

--- Assets/Scripts/Player/HelperSiro/RotatingWing.cs
+++ Assets/Scripts/Player/HelperSiro/RotatingWing.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 
 public class RotatingWing : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+
+    public void Update()
+    {
+        transform.Rotate(Vector3.right * _speed * Time.deltaTime, Space.Self);
+    }
 }
